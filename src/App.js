2fa85e3d74ab1f0c81237d42acd7f06a2b39ebab
Si <tlgyrjs@naver.com>
2024-09-11 17:51:32 +0900

    오늘 끝 ㅅㅂ

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ import MainHeader from './components/SideEffect/MainHeader/MainHeader';
 import Login from './components/SideEffect/Login/Login';
 
 const App = () => {
+  //로그인 상태를 기억하는 변수
+  const [isLoffedIn, setIsLoffedIn] = useState(false);
+
+  //서버로 로그인을 요청하는 함수. (나중에 비동기 방식으로 실제 요청이 들어갈 것.)
+  const loginHandler = (email, password) => {
+    if (email === 'abc1234' && password === 'aaa111') {
+      //로그인성공
+    } else {
+      alert('로그인 실패입니다.');
+    }
+  };
   return (
     <>
       <MainHeader />
