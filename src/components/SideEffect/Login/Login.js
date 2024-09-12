07f6b92e7a739fec9d52423121dff9e6dfbade38
Si <tlgyrjs@naver.com>
2024-09-12 16:45:13 +0900

    마지막 까지 한시간 가기전에 마킹하고가야지

--- src/components/SideEffect/Login/Login.js
+++ src/components/SideEffect/Login/Login.js
@@ -1,33 +1,51 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../../UI/Card';
 import styles from './Login.module.css';
 import ButtonComponent from '../../UI/Button';
 
-const Login = () => {
-  //이메일 입력값을 저장
+const Login = ({ onLogin }) => {
+  // 이메일 입력값을 저장
   const [enteredEmail, setEnteredEmail] = useState('');
-  //패스워드 입력값을 저장
+
+  // 패스워드 입력값을 저장
   const [enteredPw, setEnteredPw] = useState('');
-  //이메일 입력이 정상적인지 확인
+
+  // 이메일 입력이 정상적인지를 확인
   const [emailIsValid, setEmailIsValid] = useState();
+
   // 패스워드 입력도 정상적인지 확인
   const [pwIsValid, setPwIsValid] = useState();
-  // 이메일 패스워드가 둘다 동시에 정장적인 상태인지 확인.
+
+  // 이메일, 패스워드가 둘 다 동시에 정상적인 상태인지 확인.
   const [formIsValid, setFormIsValid] = useState(false);
 
-  // 이메일이 변경될때마다 실행할 헨들러
+  useEffect(() => {
+    console.log('useEffect called in login.js!');
+
+    // setTimeout을 사용해서 유효성 검증을 1초 뒤에 실행하도록 작성.
+    // 1초 이내에 새로운 입려값이 들어온다면? -> 상태변경 -> useEffect가 다시 실행됨.
+    const timer = setTimeout(() => {
+      console.log('setTimeout 호출!');
+      setFormIsValid(enteredEmail.includes('@') && enteredPw.trim().length > 6);
+    }, 1000);
+
+    // clean uo 함수 -컴포넌트가 업데이트 되거나 없어지기 직전에 실행.
+    // 사용자가 1초 이내에 추가 입력 -> 상태 변경 (컴포넌트 업데이트) -> 위에 예약한 timer를 취소해라.
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [enteredEmail, enteredPw]);
+
+  // 이메일이 변경될 때마다 실행할 핸들러
   const emailChangeHandler = (e) => {
     setEnteredEmail(e.target.value);
-
-    setFormIsValid(e.target.value.includes('@') && enteredPw.trim().length > 6);
   };
-  //비밀번호가 변경될떄 마다 실행할 헨들러
+
+  // 비밀번호가 변경될 때마다 실행할 핸들러
   const passwordChangeHandler = (e) => {
     setEnteredPw(e.target.value);
 
-    setFormIsValid(
-      e.target.value.trim().length > 6 && enteredEmail.includes('@'),
-    );
+    setFormIsValid();
   };
 
   const validateEmailHandler = () => {
@@ -38,9 +56,15 @@ const Login = () => {
     setPwIsValid(enteredPw.trim().length > 6);
   };
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log('submit 동작함!');
+    onLogin(enteredEmail, enteredPw);
+  };
+
   return (
     <Card className={styles.login}>
-      <form>
+      <form onSubmit={submitHandler}>
         <div
           className={`${styles.control} ${!emailIsValid ? styles.invalid : ''}`}
         >
@@ -50,11 +74,11 @@ const Login = () => {
             id='email'
             value={enteredEmail}
             onChange={emailChangeHandler}
-            onBlur={validateEmailHandler} // blur는  focus의 반대.
+            onBlur={validateEmailHandler} // blur는 focus의 반대.
           />
         </div>
         <div
-          className={`${styles.control}  ${!emailIsValid ? styles.invalid : ''}`}
+          className={`${styles.control} ${!pwIsValid ? styles.invalid : ''}`}
         >
           <label htmlFor='password'>Password</label>
           <input
@@ -69,7 +93,7 @@ const Login = () => {
           <ButtonComponent
             type='submit'
             className={styles.btn}
-            disabled={!formIsValid} // button은 disabled를 통해 비활섣화 가능 구현가능 true면 비활성, false면 활성
+            disabled={!formIsValid} // button은 disabled를 통해 비활성화 기능 구현. true면 비활성, false면 활성
           >
             Login
           </ButtonComponent>
