435d7c80f8d5bba8f5bfd6109a398d89645990ab
Si <tlgyrjs@naver.com>
2024-09-13 11:44:55 +0900

    뭐시기 끝

--- src/components/SideEffect/Navigation/Navigation.js
+++ src/components/SideEffect/Navigation/Navigation.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import classes from './Navigation.module.css';
+import AuthContext from '../Store/auth-context';
 
-const Navigation = ({ isLoggedIn, onLogout }) => {
+const Navigation = () => {
+  // Context store에 좀 더 쉽게 접근해서 소비할 수 있게 해주는 useContext hook.
+  // AuthContext에서 제공하는 전역 객체를 디스트럭쳐링으로 분해.
+  const { isLoggedIn, onLogout } = useContext(AuthContext);
+
   return (
     <nav className={classes.nav}>
       {isLoggedIn && (
